65e2c9a25ea0c93d9319ca724ebb5410cc751feb
= <mark.burgess.oslo.mb@gmail.com>
2021-09-14 17:50:10 +0200

    Direct query coarse particulates

---
+++ Blog/Part8/scatter.go
@@ -0,0 +1,87 @@
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	S "SST"
+	A "github.com/arangodb/go-driver"
+)
+
+// ********************************************************************************
+// * Run AFTER doors.go
+// ********************************************************************************
+
+type SMatrix struct {
+	
+	In string   `json:"In"`
+	Out string  `json:"Out"`
+	Node string `json:"Agg"`
+}
+
+// ********************************************************************************
+
+func main() {
+
+	var dbname string = "TestSST"
+	var service_url string = "http://localhost:8529"
+	var user string = "root"
+	var pwd string = "mark"
+
+	var g S.Analytics
+
+	g = S.OpenAnalytics(dbname, service_url, user, pwd)
+
+	grains := FindCoarseGrains(g)
+
+	for grain := range grains {
+
+		fmt.Printf("%20s = %s\n", grain,grains[grain])
+	}
+}
+
+//****************************************************
+
+func FindCoarseGrains(g S.Analytics) map[string][]string {
+
+	var doc SMatrix
+
+	querystring := "FOR doc1 IN Follows FOR doc2 IN Follows FILTER doc1 != doc2 && doc1._to == doc2._from RETURN { \"In\": doc1._from, \"Out\": doc2._to, Agg: doc2._from}"
+
+	cursor,err := g.S_db.Query(nil,querystring,nil)
+
+	if err != nil {
+		fmt.Println("Query failed: %v", err)
+		os.Exit(1)
+	}
+
+	defer cursor.Close()
+
+	var grains = make(map[string][]string)
+
+	for {
+		_,err = cursor.ReadDocument(nil,&doc)
+		
+		if A.IsNoMoreDocuments(err) {
+
+			break
+
+		} else if err != nil {
+
+			fmt.Println("Doc returned: %v", err)
+			os.Exit(1)
+
+		} else {
+
+			in := strings.Split(doc.In,"/")
+			out := strings.Split(doc.Out,"/")
+			key := in[1] + "->" + out[1]
+
+			grains[key] = append(grains[key],doc.Node)
+
+		}
+	}	
+
+	return grains
+}
