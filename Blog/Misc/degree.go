4cb12f30a10e7184309fc9d5e8fea7a6b0795b53
= <mark.burgess.oslo.mb@gmail.com>
2021-11-16 11:31:38 +0100

    Program to plot the degree distribution of the Internet grraph

---
+++ Blog/Misc/degree.go
@@ -0,0 +1,116 @@
+
+package main
+
+import (
+	"fmt"
+	"sort"
+
+	S "SST"
+	A "github.com/arangodb/go-driver"
+
+)
+
+// ********************************************************************************
+
+type Result struct {
+
+	K  string `json:"K"`
+	V  int    `json:"V"`
+}
+
+// ********************************************************************************
+
+func main() {
+	
+	var dbname string = "TestSST"
+	var service_url string = "http://localhost:8529"
+	var user string = "root"
+	var pwd string = "mark"
+	
+	var g S.Analytics
+	
+	g = S.OpenAnalytics(dbname, service_url, user, pwd)
+
+	// Get node degree distributions
+
+	var k_in = make(map[string]int,0)
+	var k_out = make(map[string]int,0)
+	var k = make(map[string]int,0)
+
+	GetDegreeDistribution(g,k_in,k_out,k)
+
+	//var keys map[string]int
+	var degrees []int = make([]int,0) 
+
+		fmt.Printf("\n%20s %s %s %s\n","Node", "k_in", "k_out", "k")
+
+	for name := range k {
+		degrees = append(degrees,k[name])
+		fmt.Printf("%20s %3d %3d %3d\n",name, k_in[name], k_out[name], k[name])
+	}
+
+	fmt.Println("\nSorted histogram:")
+
+	sort.Ints(degrees)
+
+	for i := len(degrees)-1; i >= 0; i-- {
+		fmt.Println(degrees[i])
+	}
+}
+
+// ********************************************************************************
+
+func GetDegreeDistribution(g S.Analytics, k_in,k_out,k map[string]int) {
+
+	var err error
+	var cursor A.Cursor
+
+	instring := "FOR n in Follows COLLECT node = n._to INTO inn RETURN { K: node, V: COUNT(inn[*])}"
+	outstring := "FOR n in Follows COLLECT node = n._from INTO out RETURN { K: node, V: COUNT(out[*])}"
+
+	cursor,err = g.S_db.Query(nil,instring,nil)
+
+	if err != nil {
+		fmt.Printf("Query failed: %v", err)
+	}
+
+	defer cursor.Close()
+
+	for {
+		var doc Result
+
+		_,err = cursor.ReadDocument(nil,&doc)
+
+		if A.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			fmt.Printf("Doc returned: %v", err)
+		} else {
+			k_in[doc.K] = doc.V
+		}
+	}
+
+	cursor,err = g.S_db.Query(nil,outstring,nil)
+
+	if err != nil {
+		fmt.Printf("Query failed: %v", err)
+	}
+
+	defer cursor.Close()
+
+	for {
+		var doc Result
+
+		_,err = cursor.ReadDocument(nil,&doc)
+
+		if A.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			fmt.Printf("Doc returned: %v", err)
+		} else {
+			k_out[doc.K] = doc.V
+			k[doc.K] = k_out[doc.K] + k_in[doc.K]
+		}
+	}
+}
+
---
+++ CAIDA/itdk_degree.go
@@ -0,0 +1,127 @@
+
+package main
+
+import (
+	"fmt"
+	"sort"
+	"math"
+	C "CAIDA_SST"
+	A "github.com/arangodb/go-driver"
+
+)
+
+// ********************************************************************************
+// Find the degree distribution of the nodes in the graph
+// ********************************************************************************
+
+type Adjacency struct {
+
+	K  string `json:"K"`
+	V  int    `json:"V"`
+}
+
+// ********************************************************************************
+
+func main() {
+	
+	var dbname string = "ITDK-snapshot-model"
+	var service_url string = "http://localhost:8529"
+	var user string = "root"
+	var pwd string = "mark"
+	
+	var g C.ITDK
+	
+	g = C.OpenITDK(dbname, service_url, user, pwd)
+
+	// Get node degree distributions from the Near relations
+
+	var k_in = make(map[string]int,0)
+	var k_out = make(map[string]int,0)
+	var Nk = make(map[int]int,0)
+
+	GetDegreeDistribution(g,k_in,k_out,Nk)
+
+	var N []int
+	var key = make(map[int]int)
+
+	// Copy the distrbution into a sortable array
+
+	for k := range Nk {
+		if Nk[k] > 0 {			
+			N = append(N,Nk[k])
+			key[Nk[k]] = k
+		}
+	}
+
+	// Sort the numbers
+
+	sort.Ints(N)
+
+	// Log-log plot to see if there is a power law line
+	
+	for k := len(N)-1; k >= 0; k-- {
+		
+		nlog := math.Log(float64(N[k]))
+		klog := math.Log(float64(key[N[k]]))
+		if nlog > 0 {
+			fmt.Printf("%10f %f\n", klog,nlog)
+		}
+	}
+}
+
+// ********************************************************************************
+
+func GetDegreeDistribution(g C.ITDK, k_in,k_out map[string]int, N map[int]int) {
+
+	var err error
+	var cursor A.Cursor
+
+	instring := "FOR n in Near COLLECT node = n._to INTO inn RETURN { K: node, V: COUNT(inn[*])}"
+	outstring := "FOR n in Near COLLECT node = n._from INTO out RETURN { K: node, V: COUNT(out[*])}"
+
+	cursor,err = g.S_db.Query(nil,instring,nil)
+
+	if err != nil {
+		fmt.Printf("Query failed: %v", err)
+	}
+
+	defer cursor.Close()
+
+	for {
+		var doc Adjacency
+
+		_,err = cursor.ReadDocument(nil,&doc)
+
+		if A.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			fmt.Printf("Doc returned: %v", err)
+		} else {
+			k_in[doc.K] = doc.V
+		}
+	}
+
+	cursor,err = g.S_db.Query(nil,outstring,nil)
+
+	if err != nil {
+		fmt.Printf("Query failed: %v", err)
+	}
+
+	defer cursor.Close()
+
+	for {
+		var doc Adjacency
+
+		_,err = cursor.ReadDocument(nil,&doc)
+
+		if A.IsNoMoreDocuments(err) {
+			break
+		} else if err != nil {
+			fmt.Printf("Doc returned: %v", err)
+		} else {
+			k_out[doc.K] = doc.V
+			k := k_out[doc.K] + k_in[doc.K]
+			N[k]++
+		}
+	}
+}
